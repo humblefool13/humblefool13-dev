0637a0d11bf552b735d3fc34bd6d078ef4af21eb
humblefool13 <akshatksyp13@gmail.com>
2024-01-03 00:38:03 +0530

    :lipstick: text done

--- src/PcInterface/homepage.jsx
+++ src/PcInterface/homepage.jsx
@@ -7,6 +7,7 @@ import {
   ambientLight,
   directionalLight,
   axesHelper,
+  useFrame,
 } from "@react-three/fiber";
 import Model from "./model.jsx";
 import { OrbitControls } from "@react-three/drei";
@@ -35,21 +36,31 @@ const Homepage = () => {
     window.addEventListener("resize", handleWindowResize);
   }, []);
   return (
-    <div className="homepage-container">
-      <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 20, 90] }}>
-        <ambientLight intensity={0.1} color={"white"} />
-        <directionalLight
-          color={"yellow"}
-          intensity={1}
-          position={[
-            cursorPosition.x - size.width / 2,
-            -(cursorPosition.y - size.height / 2),
-            100,
-          ]}
-        />
-        <OrbitControls enableZoom={false} />
-        <Model position={[0, -40, 0]} />
-      </Canvas>
+    <div className="homepage">
+      <div className="text-div">
+        <div className="text-behind">humblefool13</div>
+        <div className="text-stroked-infront">humblefool13</div>
+      </div>
+      <div className="homepage-container">
+        <Canvas
+          alpha={true}
+          id="canvas"
+          camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 20, 90] }}
+        >
+          <ambientLight intensity={0.5} color={"white"} />
+          <directionalLight
+            color={"cyan"}
+            intensity={0.9}
+            position={[
+              cursorPosition.x - size.width / 2,
+              -(cursorPosition.y - size.height / 2),
+              100,
+            ]}
+          />
+          <OrbitControls enableZoom={false} />
+          <Model position={[0, -40, 0]} rotation={[0, 0, 0]} />
+        </Canvas>
+      </div>
     </div>
   );
 };
