21970e247d9e64767700d081594a36d0197068bc
humblefool13 <akshatksyp13@gmail.com>
2024-03-10 16:19:27 +0530

    :lipstick: Bottom Bar

--- src/PcInterface/bottombar.jsx
+++ src/PcInterface/bottombar.jsx
@@ -2,7 +2,7 @@ const Bottombar = () => {
   return (
     <div className="footer-component-pc w-full mt-auto z-[99] bg-[#0f0e13]">
       <hr className="footer-rule m-auto bg-gray-700 w-[95%]"></hr>
-      <div className="footer-text text-center text-[13px] md:text-[15px] 2xl:text-[20px] font-Oswald py-[10px]">
+      <div className="footer-text text-center text-[7px] md:text-[10px] lg:text-[13px] 2xl:text-[20px] font-Oswald py-[10px]">
         Copyright © 2021-2024 humblefool13<br></br>All Rights Reserved
       </div>
     </div>
