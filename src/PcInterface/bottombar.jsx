3e186d7c25b10b04fd9b541bbf4c1b53bd28d860
humblefool13 <akshatksyp13@gmail.com>
2024-03-10 13:24:19 +0530

    bottom bar below top bar

--- src/PcInterface/bottombar.jsx
+++ src/PcInterface/bottombar.jsx
@@ -1,6 +1,6 @@
 const Bottombar = () => {
   return (
-    <div className="footer-component-pc w-full mt-auto z-[100] bg-[#0f0e13]">
+    <div className="footer-component-pc w-full mt-auto z-[99] bg-[#0f0e13]">
       <hr className="footer-rule m-auto bg-gray-700 w-[95%]"></hr>
       <div className="footer-text text-center text-[13px] md:text-[15px] 2xl:text-[20px] font-Oswald py-[10px]">
         Copyright © 2021-2024 humblefool13<br></br>All Rights Reserved
